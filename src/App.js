3067cd537d1a0a3997973ad226daee52f4cb5c7b
Nayan Hossain <nayan78934@gmail.com>
2021-01-04 20:16:24 +0600

    add folder

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import react from 'react';
+import './App.css';
+import Header from "./components/header";
+import About from './components/about';
+function App() {
+  return (
+    <div className="App">
+    < Header />
+    <About />
+    </div>
+  );
+}
+
+export default App;
