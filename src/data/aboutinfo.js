3bbdfb3850bb1ad7e7a5f03796072324899a0d1e
Nayan Hossain <48415189+nayanhossainbd@users.noreply.github.com>
2021-10-23 22:44:58 +0600

    changing info

--- src/data/aboutinfo.js
+++ src/data/aboutinfo.js
@@ -6,7 +6,7 @@ const AboutInfo = ({
   age:"21",
   phone:"+880 1704 458206",
   email:"nayanhossainbd.786@gmail.com",
-  address:"94/1 Crescent Road",
+  address:"Safipur, Kaliakair, Gazipur",
   social:{
     github:"github.com/nayanhossainbd",
     facebook:"facebook.com/nayanhossain.official",
@@ -14,7 +14,7 @@ const AboutInfo = ({
 
 
   },
-  description:'I am Junior Web developer able to build a Web presence from the ground up - from concept, navigation, layout and programming to UX and SEO. Skilled at writing well-designed, testable and efficient code using current best practices in Web development. Fast learner, hard worker and team player who is proficient in an array of scripting languages and multimedia Web tools',
+  description:'I am Web developer able to build a Web presence from the ground up - from concept, navigation, layout and programming to UX and SEO. Skilled at writing well-designed, testable and efficient code using current best practices in Web development. Fast learner, hard worker and team player who is proficient in an array of scripting languages and multimedia Web tools',
 
     
     
